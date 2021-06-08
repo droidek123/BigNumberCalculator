2fff64545901aceda1e51d6c866850b24df9da46
ak <andrzej.olszewski.2000@interia.pl>
2021-06-08 22:56:23 +0200

    Fix

--- src/main.c
+++ src/main.c
@@ -73,22 +73,16 @@ int main(void)
         if (nk_begin(ctx, "Calculator", nk_rect(0, 0, width, height), 0))
         {
             static int set = 0, prev = 0, op = 0;
-            int index_a, index_b;
-            // int
             static const char numbers[] = "789456123";
             static const char ops[] = "+-*/";
-            static int a = 0, b = 0;
-            static int *current = &a;
-              char _a[200];
-              char _b[200];
-              char _output[40000];
-              // char *_current = &_a;
+            static float a = 0, b = 0;
+            static float *current = &a;
 
             size_t i = 0;
             int solve = 0;
-            {int len; char buffer[40000];
+            {int len; char buffer[256];
             nk_layout_row_dynamic(ctx, height / 5, 1);
-            len = snprintf(buffer, 256, "%d", *current);
+            len = snprintf(buffer, 256, "%.2f", *current);
             nk_edit_string(ctx, NK_EDIT_SIMPLE, buffer, &len, 255, nk_filter_float);
             buffer[len] = 0;
             *current = atof(buffer);}
@@ -99,41 +93,14 @@ int main(void)
                     if (i > 12) continue;
                     if (nk_button_label(ctx, "C")) {
                         a = b = op = 0; current = &a; set = 0;
-                        for (int x = 0; x<200; x++){
-                          _a[x] = 0;
-                          _b[x] = 0;
-                        }
                     } if (nk_button_label(ctx, "0")) {
-                        *current = *current*10.0f;
-                        set = 0;
-                        if (current == &b) {
-                          if(index_b>=200)break;
-                            _b[index_b]= 0;
-                            index_b++;
-                        } else {
-                          if(index_a>=200)break;
-                            _a[index_a]= 0;
-                            index_a++;
-                        }
+                        *current = *current*10.0f; set = 0;
                     } if (nk_button_label(ctx, "=")) {
                         solve = 1; prev = op; op = 0;
-                        for (int x = 0; x<200; x++){
-                          printf("%c ->", _a[x]);
-                        }
-                        printf("\n");
                     }
                 } else if (((i+1) % 4)) {
                     if (nk_button_text(ctx, &numbers[(i/4)*3+i%4], 1)) {
                         *current = *current * 10.0f + numbers[(i/4)*3+i%4] - '0';
-                        if (current == &b) {
-                          if(index_b>=200)break;
-                            _b[index_b]= numbers[(i/4)*3+i%4];
-                            index_b++;
-                        } else {
-                          if(index_a>=200)break;
-                            _a[index_a]= numbers[(i/4)*3+i%4];
-                            index_a++;
-                        }
                         set = 0;
                     }
                 } else if (nk_button_text(ctx, &ops[i/4], 1)) {
